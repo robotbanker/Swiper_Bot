cfe9bab8cad4565e683086724b4a27f17e15c7f7
robotbanker <83187672+robotbanker@users.noreply.github.com>
2021-07-03 23:13:30 +0100

    Added function writing to dict bot results out of each loop

--- Swipe_Bot.py
+++ Swipe_Bot.py
@@ -1,16 +1,24 @@
+import pandas as pd
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+import collections
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 from random import uniform
 from secrets import username, password
 import json
+from pandas import DataFrame as df
 
 cck_blockers = json.loads(open('CckBlocker.json').read())
 attribute_list = []
+bot_results = {
+               'Name': [],
+               'Vote':[],
+               'Descriprion':[],
+               }
 
 class TinderBot():
     driver = webdriver.Chrome()
@@ -63,16 +71,19 @@ class TinderBot():
         swipe_right = self.driver.find_element_by_xpath(like)
         swipe_right.click()
 
+
     def subs_like (self):
         subs_like=r'//*[@id="u2005023502"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[4]'
         subs_like_btn = self.driver.find_element_by_xpath(subs_like)
         subs_like_btn.click()
+
 
     def vaccinated_like(self):
         #this function to deal with exceptions thrown by profiles with "Vaccinated" badge
         vaccinated_like = r'//*[@id="u2005023502"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[4]/button'
         vaccinated_like_btn= self.driver.find_element_by_xpath(vaccinated_like)
         vaccinated_like_btn.click()
+
 
     def dislike(self):
         dislike = r'//*[@id="u2005023502"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[2]/button'
@@ -93,23 +104,36 @@ class TinderBot():
 
 
     def selector (self):
-        sleep(3)
+        sleep(uniform(1,2))
         Name = r'//*[@id="u2005023502"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div[3]/div/div[1]/div/div/span'
         description= r'//*[@id="u2005023502"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div[3]/div/div[2]/div/div'
         name_get=self.driver.find_element_by_xpath(Name).text
-        print(name_get)
+        text_descr= self.driver.find_element_by_xpath(description).text
+
         attribute_list= self.driver.find_element_by_xpath(description).text.split(" ")
-        print(attribute_list)
+
         test= any(x in attribute_list for x in cck_blockers)
+        #trigger = set.intersection(set(attribute_list), set(cck_blockers))
+
         if test is True:
             self.dislike()
+            verdict = 'swipe left'
         else:
             self.subs_like()
+            verdict = 'swipe right'
+
+        bot_results['Name'].append(name_get)
+        bot_results['Descriprion'].append(text_descr + '_')
+        bot_results['Vote'].append(verdict)
 
     def auto_swipe(self):
+        n = int(0)
         while True:
+            n = n+1
+            df = pd.DataFrame.from_dict (bot_results, orient='index')
+            print(df.transpose())
+            #print(n)
             try:
-                sleep(uniform(2, 3))
                 self.selector()
             except Exception:
                 try:
@@ -128,7 +152,7 @@ class TinderBot():
                             except Exception:
                                     self.subs_like()
 
-
+    def file_save (self):
 
 
 
